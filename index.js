f4b97f3d40a9f69dfc09f5d68dd8f997c76df7a2
mwafrika <mwafrikajosue@gmail.com>
2019-09-25 19:07:43 +0200

    bg(heroku): setup swagger doumentation [start #168744797]

--- index.js
+++ index.js
@@ -6,13 +6,21 @@ import userRoute from './Server/routes/userRoute';
 import articleRoutes from './Server/routes/articleRoutes';
 import swagger from './documentation.json';
 
+const apiUrl = 'https://teamwork1.herokuapp.com/api/v1/api-docs/';
+
 dotenv.config();
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+// allow to show response at swaggerUI
+app.use((request, response, next) => {
+  response.header('Access-Control-Allow-Origin', '*');
+  response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 app.get('/', (req, res) => {
-  res.status(200).send({ status: 200, message: 'Welcome to Teamwork' });
+  res.status(200).send({ status: 200, message: `Welcome to Teamwork!!! to access the swagger documentation for api version 1 please follow this link ${apiUrl} ` });
 });
 app.use('/api/v1/api-docs', swagerUI.serve, swagerUI.setup(swagger));
 app.use(userRoute);
