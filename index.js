3496a37f19496e5c7b713405a3b16b0cd547dc95
mwafrika <mwafrikajosue@gmail.com>
2019-09-25 18:36:31 +0200

    bg(heroku): setup swagger doumentation [start #168744797]

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
+import swagerUI from 'swagger-ui-express';
 import userRoute from './Server/routes/userRoute';
 import articleRoutes from './Server/routes/articleRoutes';
-
+import swagger from './documentation.json';
 
 dotenv.config();
 const app = express();
@@ -13,6 +14,7 @@ app.use(bodyParser.json());
 app.get('/', (req, res) => {
   res.status(200).send({ status: 200, message: 'Welcome to Teamwork' });
 });
+app.use('/api/v1/api-docs', swagerUI.serve, swagerUI.setup(swagger));
 app.use(userRoute);
 app.use(articleRoutes);
 
