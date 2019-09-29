ccf00d60e8b909558c682f708b4a21d69075615d
mwafrika <mwafrikajosue@gmail.com>
2019-09-29 16:12:30 +0200

    chore(swagger):correct swagger error [start #166698556]

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import swagerUI from 'swagger-ui-express';
+import cors from 'cors';
 import userRoute from './Server/routes/userRoute';
 import articleRoutes from './Server/routes/articleRoutes';
 import swagger from './documentation.json';
@@ -11,7 +12,7 @@ const apiUrl = 'https://teamwork1.herokuapp.com/api/v1/api-docs/';
 dotenv.config();
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
@@ -19,7 +20,7 @@ app.get('/', (req, res) => {
 });
 
 // allow to show response at swaggerUI
-
+app.use(cors);
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-methods', 'GET, PUT, PATCH, POST, DELETE');
@@ -33,7 +34,7 @@ app.use('/api/v1/api-docs', swagerUI.serve, swagerUI.setup(swagger));
 app.use(userRoute);
 app.use(articleRoutes);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`listening to the port ${port} ...`);
 });
